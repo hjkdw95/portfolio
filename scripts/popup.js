cd30649ad16775a84ca245e645f0d40b8bfdd62a
Casey <hjkdw9506@gmail.com>
2021-07-20 16:18:30 +0900

    ♻️  popupID 개선

--- scripts/popup.js
+++ scripts/popup.js
@@ -3,6 +3,8 @@ const brunchBtn = document.querySelector(".brunch-btn");
 const carrotBtn = document.querySelector(".carrot-btn");
 const momentumBtn = document.querySelector(".momentum-btn");
 const habitBtn = document.querySelector(".habit-btn");
+const works = document.querySelector(".works");
+
 
 const interExit = document.querySelector(".interactive-exit");
 const brunchExit = document.querySelector(".brunch-exit");
@@ -10,38 +12,19 @@ const carrotExit = document.querySelector(".carrot-exit");
 const momentumExit = document.querySelector(".momentum-exit");
 const habitExit = document.querySelector(".habit-exit");
 
+let workLink = document.querySelector(".work-link");
+
 let clickNum; // 열 팝업 id 인덱스
 let shutID; // 닫을 팝업 id 인덱스
 
 
 // id 찾아오기
-function popupID() {
-    interBtn.addEventListener("click", () => {
-        clickNum = interBtn.dataset.indexNumber;
-        openPopup(clickNum);
-    });
-
-    brunchBtn.addEventListener("click", () => {
-        clickNum =brunchBtn.dataset.indexNumber;
-        openPopup(clickNum);
-    });
-
-    carrotBtn.addEventListener("click", () => {
-        clickNum = carrotBtn.dataset.indexNumber;
-        openPopup(clickNum);
-    });
-
-    momentumBtn.addEventListener("click", () => {
-        clickNum = momentumBtn.dataset.indexNumber;
-        openPopup(clickNum);
-    });
-
-    habitBtn.addEventListener("click", () => {
-        clickNum = habitBtn.dataset.indexNumber;
-        openPopup(clickNum);
-    });
+function popupID(event) {
+    let id = event.target.dataset.indexNumber;
+    if(id) {
+        openPopup(id);
+    }
 }
-
 
 // 팝업창 열기
 function openPopup(id){
@@ -60,13 +43,13 @@ function changeHeigth(id) {
             popup.childNodes[3].style.top = "145%";
             break;
         case '3' :
-            popup.childNodes[3].style.top = "180%";
+            popup.style.top = "180%";
             break;
         case '4' :
-            popup.childNodes[3].style.top = "220%";
+            popup.style.top = "220%";
             break;
         case '5' :
-            popup.childNodes[3].style.top = "275%";
+            popup.style.top = "275%";
             break;     
     }
 }
@@ -109,7 +92,7 @@ function closePopup(id){
 
 
 function init() {
-    popupID();
+    works.addEventListener("click", popupID);
     shutPopup();
 }
 
