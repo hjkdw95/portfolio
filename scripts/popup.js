76fdf69cfd351d2891ffdc2bc5ec2c87caa25659
Casey <hjkdw9506@gmail.com>
2021-07-20 16:37:24 +0900

    ♻️ refactor shutPopup function

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,21 +1,5 @@
-const interBtn = document.querySelector(".interactive-btn");
-const brunchBtn = document.querySelector(".brunch-btn");
-const carrotBtn = document.querySelector(".carrot-btn");
-const momentumBtn = document.querySelector(".momentum-btn");
-const habitBtn = document.querySelector(".habit-btn");
 const works = document.querySelector(".works");
-
-
-const interExit = document.querySelector(".interactive-exit");
-const brunchExit = document.querySelector(".brunch-exit");
-const carrotExit = document.querySelector(".carrot-exit");
-const momentumExit = document.querySelector(".momentum-exit");
-const habitExit = document.querySelector(".habit-exit");
-
-let workLink = document.querySelector(".work-link");
-
-let clickNum; // 열 팝업 id 인덱스
-let shutID; // 닫을 팝업 id 인덱스
+const popups = document.querySelector(".popups");
 
 
 // id 찾아오기
@@ -23,6 +7,7 @@ function popupID(event) {
     let id = event.target.dataset.indexNumber;
     if(id) {
         openPopup(id);
+        changeHeigth(id);
     }
 }
 
@@ -30,56 +15,35 @@ function popupID(event) {
 function openPopup(id){
     let popup = document.getElementById(`${id}`);
     popup.classList.remove("disappear");
-    changeHeigth(id);
 }
 
 // 각 팝업창 높이 조절
 function changeHeigth(id) {  
-    let popup = document.getElementById(`${id}`);  
+    let popupStyle = document.getElementById(`${id}`).childNodes[3];  
     switch(id) {
         case '1' : 
             break;
         case '2' :
-            popup.childNodes[3].style.top = "145%";
+            popupStyle.style.top = "145%";
             break;
         case '3' :
-            popup.style.top = "180%";
+            popupStyle.style.top = "185%";
             break;
         case '4' :
-            popup.style.top = "220%";
+            popupStyle.style.top = "225%";
             break;
         case '5' :
-            popup.style.top = "275%";
+            popupStyle.style.top = "265%";
             break;     
     }
 }
 
 // 닫을 팝업창 id 찾기
-function shutPopup() {
-    interExit.addEventListener("click", (event) => {
-        shutID = event.target.parentNode.parentNode.parentNode.id;
-        closePopup(shutID);
-    })
-
-    brunchExit.addEventListener("click", (event) => {
-        shutID = event.target.parentNode.parentNode.parentNode.id;
-        closePopup(shutID);
-    })
-
-    carrotExit.addEventListener("click", (event) => {
-        shutID = event.target.parentNode.parentNode.parentNode.id;
-        closePopup(shutID);
-    })
-
-    momentumExit.addEventListener("click", (event) => {
-        shutID = event.target.parentNode.parentNode.parentNode.id;
-        closePopup(shutID);
-    })
-
-    habitExit.addEventListener("click", (event) => {
-        shutID = event.target.parentNode.parentNode.parentNode.id;
-        closePopup(shutID);
-    })
+function shutPopup(event) {
+    let id = event.target.parentNode.parentNode.parentNode.id;
+    if(id) {
+        closePopup(id);
+    }
 }
 
 
@@ -93,7 +57,7 @@ function closePopup(id){
 
 function init() {
     works.addEventListener("click", popupID);
-    shutPopup();
+    popups.addEventListener("click", shutPopup);
 }
 
 init();
