3a98810bd8886fec328570bcf8618cf2f2278a38
Casey <hjkdw9506@gmail.com>
2021-07-26 15:42:50 +0900

    ⚡️ [add] mobile page fade in effects (skills)

--- scripts/title-up.js
+++ scripts/title-up.js
@@ -3,3 +3,28 @@ const mainLetter = document.querySelector(".main-letter");
 window.onload = function() {
     mainLetter.classList.add("letter-up");
 }
+
+// mobile skills 부분 fade in 효과
+function fadeIn() {
+    const js = document.querySelector("#js");
+    const communication = document.querySelector("#com");
+
+    let scroll = document.documentElement.scrollTop;
+
+    if(scroll > 0) {
+        js.classList.add("boxup");
+    }
+    if(scroll > 160) {
+        communication.classList.add("boxup");
+    }
+}
+
+
+
+// 모바일 화면일때만 가능하도록 하기
+let mql = window.matchMedia("screen and (max-width: 1024px)");
+
+if(mql.matches) {
+    window.addEventListener("scroll", fadeIn);
+};
+
