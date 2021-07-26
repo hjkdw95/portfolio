49532fb552a7454d982b0c96ce632d835ebb4eda
Casey <hjkdw9506@gmail.com>
2021-07-26 12:34:58 +0900

    ⚡️[fix] 팝업창 위치 고정

--- scripts/popup.js
+++ scripts/popup.js
@@ -7,7 +7,6 @@ function popupID(event) {
     let id = event.target.dataset.indexNumber;
     if(id) {
         openPopup(id);
-        changeHeigth(id);
     }
 }
 
@@ -17,26 +16,6 @@ function openPopup(id){
     popup.classList.remove("disappear");
 }
 
-// 각 팝업창 높이 조절
-function changeHeigth(id) {  
-    let popupStyle = document.getElementById(`${id}`).childNodes[3];  
-    switch(id) {
-        case '1' : 
-            break;
-        case '2' :
-            popupStyle.style.top = "145%";
-            break;
-        case '3' :
-            popupStyle.style.top = "185%";
-            break;
-        case '4' :
-            popupStyle.style.top = "225%";
-            break;
-        case '5' :
-            popupStyle.style.top = "265%";
-            break;     
-    }
-}
 
 // 닫을 팝업창 id 찾기
 function shutPopup(event) {
